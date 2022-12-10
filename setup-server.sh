f3996f4a47a7b4b466bd7f22d7aa5604cbd9c127
stho32 <stho32github@gmail.com>
2022-12-10 18:34:05 +0100

    Adding a new user is not done in the setup-server script anymore.

--- setup-server.sh
+++ setup-server.sh
@@ -3,14 +3,25 @@
 # Set the name of the user to add
 user_name=stho32
 
-# Call the 'add_user.sh' script to add the user to the system
-bash ./scripts/add_user.sh $user_name
+# Use the 'id' command to check if the user exists
+user_exists=$(id $user_name)
+
+# Check if the user exists
+if [ -z "$user_exists" ]; then
+    # If the user does not exist, use the 'adduser' command to add the user to the system
+    adduser $user_name
+else
+    # If the user exists, print a message to the console
+    echo "The user '$user_name' already exists on the system"
+fi
+
 
 # Check if the user is part of the 'sudo' group
 if ! groups $user_name | grep -qw "sudo"; then
     # Add the user to the 'sudo' group
     sudo usermod -aG sudo $user_name
 fi
+
 
 # Check if the 'PermitRootLogin' option is set to 'yes' in the '/etc/ssh/sshd_config' file
 if grep -q "^PermitRootLogin yes" /etc/ssh/sshd_config; then
