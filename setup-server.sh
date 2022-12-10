65edc4d88407e15dac65e816cecd9bed4c67bb43
stho32 <stho32github@gmail.com>
2022-12-10 18:58:10 +0100

    another try

--- setup-server.sh
+++ setup-server.sh
@@ -34,7 +34,7 @@ fi
 
 # Copy the script over to the user and execute it in his name
 cp ./setup-as-user.sh /home/$user_name
-chown username /home/$user_name/setup-as-user.sh
+chown $user_name /home/$user_name/setup-as-user.sh
 su -c /home/$user_name/setup-as-user.sh $user_name
 
 echo "setup complete"
